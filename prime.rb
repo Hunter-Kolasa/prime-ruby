513780c055b8bec0be0ba44c6cbd927556e9f76f
Hunter <kolasa.hunter@aol.com>
2020-11-12 21:51:07 -0500

    commit tree b5d83b221d8bc520030452ab291878e24e471ba6

--- prime.rb
+++ prime.rb
@@ -1,1 +1,31 @@
-# Add  code here!
+## works in repl.it but getting infinite false loop when running learn test??
+# def prime?(integer)
+#   n = 2
+#    divisible = []
+#     prime = []
+#     while divisible.length == 0 && prime.length == 0
+#       if integer.abs != integer || integer == 0
+#         puts false
+#       elsif integer % n == 0
+#         puts false
+#         divisible << integer
+#       elsif n >= integer / 2
+#         puts true
+#         prime << integer
+#       else
+#         n += 1
+#       end
+#     end
+# end
+
+def prime?(int)
+    if int <= 1
+        false
+    elsif int == 2
+        true
+    else
+        (2..int/2).none? {|i| int % i == 0}
+    end
+end
+
+        
